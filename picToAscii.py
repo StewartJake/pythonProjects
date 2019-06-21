3a167eef3b339f453349ba858506618118932560
Jacob Stewart <jacobstewart1987@gmail.com>
2019-06-20 21:37:38 -0700

    trimmed .py code

--- picToAscii.py
+++ picToAscii.py
@@ -2,10 +2,11 @@ from PIL import Image
 import numpy as np
 import math
 
-im = Image.open("20190616_193114.jpg")
+im = Image.open("pineapple.jpg")
 print("Image Successfully Loaded" + '\n' + "Image Size: {0}x{1}".
         format(im.size[0], im.size[1]))
 
+# if want aspect ratio maintained
 height, width = im.size
 aspectRatio = height/width
 # change for terminal sizes
@@ -19,7 +20,6 @@ print("Image Successfully Resized" + '\n' + "Image Size: {0}x{1}".
 rows, cols = im.size
 px = im.load()
 pxMap = np.zeros((rows,cols), dtype=tuple)
-#pxMap2 = np.zeros((rows,cols), dtype=tuple)
 
 # Pixel Object not iterable
 # convert to numpy arr
@@ -41,17 +41,9 @@ for i in range(rows):
     for j in range(cols):
         index = int(pxMap[i, j]*(65/256))
         pxMap[i,j] = asciiMap[index]
-#        pxMap2[i, j] = (asciiMap[index],)*3
 
 # recreate image in ascii
 for i in range(rows):
     for j in range(cols):
         print(pxMap[i, j], end ="")
     print('\n', end = "")
-
-
-#im2 = Image.fromarray(pxMap)
-#im2.show()
-
-
-
