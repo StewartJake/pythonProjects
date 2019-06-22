9f7011683153cfb2b6abd2ede4d2fae35e0d158c
Jacob Stewart <jacobstewart1987@gmail.com>
2019-06-22 10:06:32 -0700

    printBoard function; started updateBoard

--- gameOfLife/gameOfLife.py
+++ gameOfLife/gameOfLife.py
@@ -1,36 +1,47 @@
 import random
+import copy
 import pdb
 
 log = open("errorLog","w+")
 
-height = 5;
-width = 5;
-alive = 1;
-dead = 0;
+height = 10
+width = 10
+alive = 1
+dead = 0
 
 # a tuple of tuples with dim width x height
 deadState = [[dead]*width for _ in range(height)]
 
 #pdb.set_trace();
 # randomize the boardState
-randomState = deadState;
+randomState = copy.deepcopy(deadState)
+
 for i in range(height):
     for j in range(width):
-        randomState[i][j] = random.randint(0,1);
+        randomState[i][j] = random.randint(0,1)
 
-# print board state like a board
-rowStr = ""
-for i in range(height):
-    for j in range(width):
-        if (randomState[i][j] == alive):
-            rowStr += "@";
-        else:
-            rowStr += " ";
-    rowStr += "\n";
+def  printBoard(board):
+    # print board state like a board
+    rowStr = ""
+    for i in range(height):
+        for j in range(width):
+            if (board[i][j] == alive):
+                rowStr += "@"
+            else:
+                rowStr += " "
+        rowStr += "\n"
 
-print(randomState, file = log);
+    print(rowStr)
+
+def updateBoard(board)
+    # updateBoard will update every cell according to the 4 rules
+    # parameter board is just a list of lists
+    updatedBoard = copy.deepcopy(board)
+    for i in range(len(board)):
+        for j in range(len(board[i])):
+            neighborSum = 0
 
 
-print(rowStr);
-
-log.close();
+printBoard(deadState)
+printBoard(randomState)
+log.close()
