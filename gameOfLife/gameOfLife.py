4508b74594c215d003b097690c3550b8e38f70d0
Jacob Stewart <jacobstewart1987@gmail.com>
2019-06-22 08:53:26 -0700

    Added some print

--- gameOfLife/gameOfLife.py
+++ gameOfLife/gameOfLife.py
@@ -1,17 +1,36 @@
 import random
+import pdb
 
 log = open("errorLog","w+")
 
-height = 5
-width = 5
+height = 5;
+width = 5;
+alive = 1;
+dead = 0;
 
 # a tuple of tuples with dim width x height
-deadState = (((0,)*width),)*height
+deadState = [[dead]*width for _ in range(height)]
 
-randomState = deadState
+#pdb.set_trace();
+# randomize the boardState
+randomState = deadState;
 for i in range(height):
     for j in range(width):
-        randomState[i][j] = random.randint(0,1)
+        randomState[i][j] = random.randint(0,1);
 
-print(randomState, file = log)
+# print board state like a board
+rowStr = ""
+for i in range(height):
+    for j in range(width):
+        if (randomState[i][j] == alive):
+            rowStr += "@";
+        else:
+            rowStr += " ";
+    rowStr += "\n";
 
+print(randomState, file = log);
+
+
+print(rowStr);
+
+log.close();
