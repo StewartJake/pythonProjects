fc6e53ce5851ea403cdc31d7c3f4c3ae7093e443
Jacob Stewart <jacobstewart1987@gmail.com>
2019-06-23 09:43:11 -0700

    fixed asciiPic folder

---
+++ asciiPic/README.md
@@ -0,0 +1,7 @@
+picToAscii.py
+
+-change the im variable to an image you want to convert to ascii
+
+-change height
+
+-run *.py
---
+++ asciiPic/pictoAscii.py
@@ -0,0 +1,49 @@
+from PIL import Image
+import numpy as np
+import math
+
+im = Image.open("20190616_193114.jpg")
+print("Image Successfully Loaded" + '\n' + "Image Size: {0}x{1}".
+        format(im.size[0], im.size[1]))
+
+# if want aspect ratio maintained
+height, width = im.size
+aspectRatio = height/width
+# change for terminal sizes
+desiredHeight =640
+formulatedWidth =  math.ceil(desiredHeight/aspectRatio)
+
+im = im.resize((desiredHeight, formulatedWidth))
+print("Image Successfully Resized" + '\n' + "Image Size: {0}x{1}".
+        format(im.size[0], im.size[1]))
+
+rows, cols = im.size
+px = im.load()
+pxMap = np.zeros((rows,cols), dtype=tuple)
+
+# Pixel Object not iterable
+# convert to numpy arr
+for i in range(rows):
+    for j in range(cols):
+        pxMap[i,j] = px[i,j]
+
+# assigns a luminosity score to each pixel
+for i in range(rows):
+    for j in range(cols):
+        r,g,b = pxMap[i, j]
+        pxMap[i,j] = (r + g + b) / 3
+        #pxMap[i, j] = 0.21*r + 0.72*g +0.07*b
+
+asciiMap = "`^\",:;Il!i~+_-?][}{1)(|\\/tfjrxnuvczXYUJCLQ0OZmwqpdbkhao*#MW&8%B@$"
+
+# changes luminosity score to ascii by weights (65/256)
+for i in range(rows):
+    for j in range(cols):
+        index = int(pxMap[i, j]*(65/256))
+        pxMap[i,j] = asciiMap[index]
+
+# recreate image in ascii
+for i in range(rows):
+    for j in range(cols):
+        print(pxMap[i, j], end ="")
+    print('\n', end = "")
