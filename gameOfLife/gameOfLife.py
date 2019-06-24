63e0b847704ea110a664f1d50ebc33686c0d6f7a
Jacob Stewart <jacobstewart1987@gmail.com>
2019-06-23 21:30:53 -0700

    started load fn/user input

--- gameOfLife/gameOfLife.py
+++ gameOfLife/gameOfLife.py
@@ -1,11 +1,13 @@
 import random
 import copy
 import pdb
+import sys
 
 ALIVE = 1
 DEAD = 0
 
 log = open("errorLog","w+")
+saveLog = open("saves","r")
 height = 38 
 width = 150
 # a tuple of tuples with dim width x height
@@ -20,8 +22,8 @@ for i in range(height):
 def  printBoard(board):
     # print board state like a board
     rowStr = ""
-    for i in range(height):
-        for j in range(width):
+    for i in range(len(board)):
+        for j in range(len(board[i])):
             if (board[i][j] == ALIVE):
                 rowStr += u"\u2588" 
             else:
@@ -41,6 +43,8 @@ def updateBoard(board):
 def findNeighbors(board):
     #board will be a list and this function will return a sum of neighbors
     neighborBoard = copy.deepcopy(board)
+    ALIVE = 1
+    DEAD = 0
     for i in range(len(board)):
         for j in range(len(board[i])):
             neighborSum = 0
@@ -108,19 +112,47 @@ def updateBoard(board):
             if (board[i][j] == DEAD) and (neighBoard[i][j] == 3):
                     nextBoard[i][j] = ALIVE
     return nextBoard
+
+def loadBoard(key):
+    # take a key, as a string, to search the save file
+    # returns a tuple whether it was found and if so also a board
+    found = False
+    f = open("saves","r")
+    lineList = f.readlines()
+    board = [[]]
+#    pdb.set_trace()
+    for line in lineList:
+        if (not line.startswith("#") and key in line):
+            ind = lineList.index(line)
+            board = lineList[ind + 1].split('\n')      #relies on each line having an eol
+            found = True
+    return (found, eval(board[0]))
+
 # attempts to end program if just stuck in same pattern
 #still needs work
 formerState = copy.deepcopy(deadState)
 formerState2 = copy.deepcopy(formerState)
 i = 0
-while ((randomState != formerState) and (randomState != formerState2)):
-    formerState = randomState
+# add some sanitation to this input
+initialState = randomState
+wantLoaded = input("Do you want to load a file?" + '\n' + "(Y/N):\n")
+if (wantLoaded.lower() == "y"):
+    boardChoice = input("What is the save file:\n")
+    gameStart = loadBoard(boardChoice)
+    if (gameStart[0]):
+        initialState = gameStart[1]
+    else:
+        print("Board not found in save file.")
+        sys.exit()
+
+while ((initialState != formerState) and (initialState != formerState2)):
+    formerState = initialState
     if (i%2 ==1):
-        formerstate2 = randomState
+        formerstate2 = initialState
 #    pdb.set_trace()
-    randomState = updateBoard(randomState)
-    print(randomState,file = log)
-    printBoard(randomState)
+    initialState = updateBoard(initialState)
+    print(initialState,file = log)
+    printBoard(initialState)
     i += 1
 
 log.close()
