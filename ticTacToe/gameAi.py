8f61143029b2a90c085b65add1dafecbbd7c0c1b
Jacob Stewart <jacobstewart1987@gmail.com>
2019-06-27 08:15:00 -0700

    Started AI

---
+++ ticTacToe/gameAi.py
@@ -0,0 +1,6 @@
+import tttFunctions
+
+# All the AI funvtions will take in 2 arguments:
+# board and symbol of the person who's turn it is
+# Return will be the coordinate of the move
+def randomAi(board, symbol):
