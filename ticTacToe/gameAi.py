d16a2d113d50fb486d4fd40f708f8ebce05518a9
Jacob Stewart <jacobstewart1987@gmail.com>
2019-06-27 22:13:28 -0700

    winningAi

--- ticTacToe/gameAi.py
+++ ticTacToe/gameAi.py
@@ -20,8 +20,48 @@ def legalMoves(board):
                         break
     return legalMoves
 
+
 def randomAi(board, symbol):
     possibleMoves = legalMoves(board)
     choice = random.randrange(0, len(possibleMoves))
     return possibleMoves[choice]
 
+
+def findWinningAi(board, symbol):
+    winningMoves = [] 
+    # Rows
+    for r in range(len(board)):
+        for c in range(len(board[r])):
+            if board[0][c] == symbol and board[1][c] == symbol:
+                winningMoves.append((2,c))
+            elif board[0][c] == symbol and board[2][c] == symbol:
+                winningMoves.append((1,c))
+            elif board[1][c] == symbol and board[2][c] == symbol:
+                winningMoves.append((0,c))
+    # Columns
+    for r in range(len(board)):
+        if board[r][0] == symbol and board[r][1] == symbol:
+            winningMoves.append((r,2))
+        elif board[r][1] == symbol and board[r][2] == symbol:
+            winningMoves.append((r,0))
+        elif board[r][0] == symbol and board[r][2] == symbol:
+            winningMoves.append((r,1))
+    # Diagonals
+    if board[1][1] == symbol:
+        if board[0][0] == symbol:
+            winningMoves.append((2,2))
+        elif board[2][2] == symbol:
+            winningMoves.append((0,0))
+        elif board[2][0] == symbol:
+            winningMoves.append((0,2))
+        elif board[0][2] == symbol:
+            winningMoves.append((2,0))
+    if len(winningMoves) > 0:
+        for key, val in strFile.choiceMap.items():
+            if val == winningMoves[0]:
+                choice = key
+                break
+    else:
+        choice = randomAi(board, symbol)
+    return choice
+        
