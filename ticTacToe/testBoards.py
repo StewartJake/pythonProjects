41676e0121ea1348c932d5179132d9d95d2f00b2
Jacob Stewart <jacobstewart1987@gmail.com>
2019-06-30 16:08:57 -0700

    test files

--- ticTacToe/testBoards.py
+++ ticTacToe/testBoards.py
@@ -6,7 +6,10 @@ o1Away = [['O','O','X'],['O','X','O'],[None, 'X', None]]
 x2Away = [[None,'O',None], ['X','X','O'], [None, 'X', None]]
 x1Space = [['X','O','O'],['X','O','O'],[None,'X','X']]
 xWin = [['X','X','X'],[None,None,None],[None,None,None]]
-oWins =[['O','O','O'],[None,None,None],[None,None,None]] 
+oWins = [['O','O','O'],[None,None,None],[None,None,None]] 
+oFork = [['X','X', 'O'],[None,'O','O'],['X',None,None]]
+xDrawLoss = [['X','O','X'],[None,'X',None],['O',None,'O']]
+
 
 def tests():
     print("-X    True")
