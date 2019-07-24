e5ac80df01cff8a42151c778c34e65f90d3e045e
Jacob Stewart <jacobstewart1987@gmail.com>
2019-07-24 07:17:58 -0700

    added QuickUnion 1 step

--- algs/UnionFind.py
+++ algs/UnionFind.py
@@ -25,3 +25,25 @@ class WeightedQuickUnion:
         else:
             self.ind[q_root] = p_root
             self.sz[p_root] += self.sz[q_root]
+
+
+class QuickUnionPC1:
+
+    def __init__(self, n):    
+        self.ind = []
+        for i in range(n):
+            self.ind.append(i)
+
+    def root(self, i):
+        self.ind[i] = self.ind[self.ind[i]]
+        while i != self.ind[i]:
+            i = self.ind[i]
+        return i
+
+    def connected(self, p, q):
+        return self.root(p) == self.root(q)
+
+    def union(self, p, q):
+        p_root = self.root(p)
+        q_root = self.root(q)
+        self.ind[q_root] = p_root
